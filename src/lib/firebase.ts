ed31216db2b2f20d4b836402fb9d6f751e97380d
Vladimir Danila <matrezz.one@gmail.com>
2025-09-20 21:00:46 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAqREgOLG1leZY8ffJpTiQWTqkybVYfzYE",
+  authDomain: "studio-7892987648-351fe.firebaseapp.com",
+  projectId: "studio-7892987648-351fe",
+  storageBucket: "studio-7892987648-351fe.appspot.com",
+  messagingSenderId: "477632953392",
+  appId: "1:477632953392:web:35f17d200d4a3cb5bfc1f5",
 };
 
 // Initialize Firebase
